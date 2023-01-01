5474392983c6d8852393d81a543cbd71db63aa56
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-01-01 17:31:11 -0500

    Dataset updated

--- city_app.R
+++ city_app.R
@@ -2,24 +2,26 @@ require(shiny)
 require(ggplot2)
 library(DT)
 library(leaflet)
+library(feather)
 
 ## Global
 
 ### Distance matrix
-n <- 20
-x <- data.frame(rnorm(n))
-m <- data.frame(t(combn(row.names(x),2)), as.numeric(dist(x)))
-names(m) <- c("c1", "c2", "distance")
+#x <- feather::read_feather("data/pca_dists2.feather")
+#x <- data.frame(x)
+#row.names(x) <- colnames(x)
+#xy <- t(combn(colnames(x), 2))
+#test <- data.frame(xy, dist=x[xy])
+#names(test) <- c("c1", "c2", "distance")
+#write_feather(test, "data/pca_dists2.long.feather")
+m <- feather::read_feather("data/pca_dists2.long.feather")
 
 ### City characteristics
+City_chars <- read.csv("data/cities_stabilities_k6.csv")
+colnames(City_chars)[2] <- "Continent"
 
-City_chars <-    cbind.data.frame(City = 1:n, 
-                                  Cluster = sample(LETTERS[1:6], n, replace = TRUE),
-                                  Continent = sample(LETTERS[1:7], n, replace = TRUE)
-)
-
-coords <- read.csv("cities_coords.csv")
-City_chars <- cbind.data.frame(City_chars, coords[1:n,c("x", "y")])
+coords <- read.csv("data/cities_coords.csv")
+City_chars <- cbind.data.frame(City_chars, coords)
 
 
 ### UI
@@ -34,7 +36,6 @@ ui <- fluidPage(
       width = 9
     ),
     sidebarPanel(
-      uiOutput(outputId = "modelFilter"),
       uiOutput(outputId = "ClusterFilter"),
       width = 3
     )
@@ -51,27 +52,29 @@ server <- function(input, output) {
   
   # Get distanceTab
   output$distanceTab <- renderUI({
-    selectInput(
+    selectizeInput(
       inputId = "city",
       label = "Select a city",
-      choices = c("", as.character(unique(City_chars$City)))
+      choices = c("", as.character(unique(City_chars$City))),
+      selected = 1,
+      options = list(maxOptions = 100)
     )})
   
   # Subset data by city.
   df2 <-
     reactive({
-      df2 <- droplevels(m[m$c1 == input$city | m$c2 == input$city,])})
+      df2 <- droplevels(m[m$c1 == input$city | m$c2 == input$city,])
+      })
+  
   # Filter data.
   df3 <-
     eventReactive({
-      input$modelFilter
       input$clusterFilter
       df2()
     },
     {
-        req(input$modelFilter) #input$clusterFilter
+        req(input$clusterFilter) #input$clusterFilter
         
-        sub <- City_chars[City_chars$Continent %in% input$modelFilter,]
         sub <- City_chars[City_chars$Cluster %in% input$clusterFilter,]
         
         df3a <- df2()[df2()$c1 %in% sub$City | df2()$c2 %in% sub$City,]
@@ -84,17 +87,6 @@ server <- function(input, output) {
   # Plot table.
   output$table <- DT::renderDataTable({
     datatable(df3(), filter = 'top')
-  })
-  
-  # Filter by continent / cluster
-  output$modelFilter <- renderUI({
-    req(input$city)
-    checkboxGroupInput(
-      inputId = "modelFilter",
-      label = "Filter by continent",
-      choices = as.character(unique(City_chars$Continent)),
-      selected = as.character(unique(City_chars$Continent))
-    )
   })
   
   output$ClusterFilter <- renderUI({
@@ -115,7 +107,7 @@ server <- function(input, output) {
   filtered_table2 <- reactive({
     req(input$table_rows_all)
     subsetCities <- filtered_table()
-    tCities <- as.numeric(unique(unlist(subsetCities[,c(1,2)])))
+    tCities <- unique(unlist(subsetCities[,c(1,2)]))
     tCities <- tCities[which( tCities !=  input$city)]
     City_chars[City_chars$City %in% tCities,]
   })
