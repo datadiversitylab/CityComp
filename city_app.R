cc942f4d06c71226828c9017c470155903d15e01
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-03-12 18:43:03 -0700

    Melting the dataset

--- city_app.R
+++ city_app.R
@@ -1,15 +1,18 @@
 require(shiny)
 require(ggplot2)
 library(DT)
+library(data.table)
 library(leaflet)
 library(feather)
 library(shinydashboard)
 library(shinybusy)
 library(dashboardthemes)
+library(reshape2)
 
 ## Global
 #m <- read_csv("data/pca_dists2.csv.gz")
 m <- fread("data/pca_dists2.csv.gz")
+m <- na.omit(melt(m))
 
 ### City characteristics
 City_chars <- read.csv("data/cities_stabilities_k6.csv")
@@ -18,9 +21,7 @@ colnames(City_chars)[2] <- "Continent"
 coords <- read.csv("data/cities_coords.csv")
 City_chars <- cbind.data.frame(City_chars, coords)
 
-
 ### UI
-
 header <- dashboardHeader(title = "City comparison",
                           titleWidth = 200)
 
@@ -44,15 +45,9 @@ body <- dashboardBody(
                uiOutput("cities"),
                uiOutput("clusters")
            ),
-           box(width = NULL, title = tagList(shiny::icon("gear",class = 'fa-lg'), "Run") ,
-               solidHeader = T, collapsible = T, status = 'primary',
-               "Once you have selected the city and clusters, please make sure you run the filter:",
-               br(),
-               actionButton("method", "Run")
-           ),
            box(width = NULL, title = tagList(shiny::icon("laptop-code",class = 'fa-lg'), "Code") ,
                solidHeader = T, collapsible = T, status = 'primary',
-               a(icon("github",class = 'fa-lg'), ' GitHub repository', href = 'https://github.com/cromanpa94/vessels.app', target = "_blank"))
+               a(icon("github",class = 'fa-lg'), ' GitHub repository', href = 'https://github.com/cromanpa94/city_app', target = "_blank"))
     ),
     column(width = 8,
            box(width = NULL, solidHeader = TRUE,
@@ -72,89 +67,36 @@ ui <- dashboardPage(skin = 'black',
 ## Server
 
 server <- function(input, output) {
-  # Read data.
-  df <- m
-  
-  # Get distanceTab
+
+  # Get filters
   output$cities <- renderUI({
     selectizeInput(
       inputId = "cities",
       label = "Select a city",
-      choices = c("", as.character(unique(colnames(m)[-1], m$c1 ))),
-      selected = 2,
+      choices = unique(m[,1]),
+      selected = 1,
       options = list(maxOptions = 100)
     )})
-  
   
   output$clusters <- renderUI({
     selectizeInput(
       inputId = "clusters",
       label = "Select a cluster",
-      choices = c("", as.character(unique(City_chars$Cluster))),
+      choices = c(as.character(unique(City_chars$Cluster))),
       selected = 1,
       options = list(maxOptions = 100)
     )})
   
-  # Subset data
-  df2 <-
-    reactive({
-      df2 <- droplevels(m[m$c1 == input$city | m$c2 == input$city,])
-      })
+  # Filter the table
+  table <- reactive({
+    d1 <- m[m$c1 == input$cities,]
+    d1
+  })
   
-  # Filter data.
-  df3 <-
-    eventReactive({
-      input$clusterFilter
-      df2()
-    },
-    {
-        req(input$clusterFilter) #input$clusterFilter
-        
-        sub <- City_chars[City_chars$Cluster %in% input$clusterFilter,]
-        
-        df3a <- df2()[df2()$c1 %in% sub$City | df2()$c2 %in% sub$City,]
-
-      df3a
-    },
-    ignoreNULL = FALSE,
-    ignoreInit = TRUE)
-  
-  # Plot table.
+  # Plot table
   output$table <- DT::renderDataTable({
-    datatable(df3(), filter = 'top')
+    datatable(table(), filter = 'top')
   })
-  
-  output$ClusterFilter <- renderUI({
-    req(input$city)
-    checkboxGroupInput(
-      inputId = "clusterFilter",
-      label = "Filter by cluster",
-      choices = as.character(unique(City_chars$Cluster)),
-      selected = as.character(unique(City_chars$Cluster))
-    )
-  })
-  
-  filtered_table <- reactive({
-    req(input$table_rows_all)
-    m[input$table_rows_all, ]  
-  })
-  
-  filtered_table2 <- reactive({
-    req(input$table_rows_all)
-    subsetCities <- filtered_table()
-    tCities <- unique(unlist(subsetCities[,c(1,2)]))
-    tCities <- tCities[which( tCities !=  input$city)]
-    City_chars[City_chars$City %in% tCities,]
-  })
-
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = filtered_table2(), lng = ~x, lat = ~y)
-  })
-  
   
 }
 
