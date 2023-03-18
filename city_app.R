29f04d415887c2127c1cd187a70dd30b2a80ad59
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-03-18 16:13:12 -0700

    First map inset

--- city_app.R
+++ city_app.R
@@ -8,17 +8,19 @@ library(shinydashboard)
 library(shinybusy)
 library(dashboardthemes)
 library(reshape2)
+library(shinyWidgets)
 
 ## Global
 #m <- read_csv("data/pca_dists2.csv.gz")
 m <- fread("data/pca_dists2.csv.gz")
+namesds <- unlist(c(m[1,1], colnames(m)[-1]))
 
 ### City characteristics
 City_chars <- read.csv("data/cities_stabilities_k6.csv")
 colnames(City_chars)[2] <- "Continent"
 
 coords <- read.csv("data/cities_coords.csv")
-City_chars <- cbind.data.frame(City_chars, coords)
+City_chars <- cbind.data.frame(namesds,City_chars, coords)
 
 ### UI
 header <- dashboardHeader(title = "City comparison",
@@ -42,15 +44,20 @@ body <- dashboardBody(
            box(width = NULL, title = tagList(shiny::icon("filter",class = 'fa-lg'), "Filters") ,
                solidHeader = T, collapsible = T, status = 'primary',
                uiOutput("cities"),
+               uiOutput("continent"),
                uiOutput("clusters")
            ),
+           box(width = NULL, title = tagList(shiny::icon("laptop-code",class = 'fa-lg'), "Map") ,
+               solidHeader = T, collapsible = FALSE, status = 'primary',
+               leafletOutput('Map',height = 200)),
            box(width = NULL, title = tagList(shiny::icon("laptop-code",class = 'fa-lg'), "Code") ,
-               solidHeader = T, collapsible = T, status = 'primary',
-               a(icon("github",class = 'fa-lg'), ' GitHub repository', href = 'https://github.com/cromanpa94/city_app', target = "_blank"))
+               solidHeader = T, collapsible = FALSE, status = 'primary',
+               a(icon("github",class = 'fa-lg'), ' GitHub repository', href = 'https://github.com/cromanpa94/city_app', target = "_blank")
+               )
     ),
     column(width = 8,
            box(width = NULL, solidHeader = TRUE,
-               DT::dataTableOutput("table")
+               DT::dataTableOutput("table"),
            )
     )
   )
@@ -69,26 +76,50 @@ server <- function(input, output) {
   
   # Get filters
   output$cities <- renderUI({
-    selectInput(
+    pickerInput(
       inputId = "cities",
       label = "Select a city",
-      choices = c(Choose = unique(as.vector(m[,1]))),
-      selectize = TRUE
+      choices = unique(City_chars$namesds),
+      options = list(`live-search`=TRUE)
     )})
   
   output$clusters <- renderUI({
-    selectizeInput(
+    pickerInput(
       inputId = "clusters",
-      label = "Select a cluster",
+      label = "Select a  clusters",
       choices = c(as.character(unique(City_chars$Cluster))),
-      selected = 1,
-      options = list(maxOptions = 100)
+      selected = c(as.character(unique(City_chars$Cluster))),
+      options = list(`actions-box` = TRUE),
+      multiple = TRUE
     )})
+  
+  
+  output$continent <- renderUI({
+    pickerInput(
+      inputId = "continent",
+      label = "Select a  continent",
+      choices = c(as.character(unique(City_chars$Continent))),
+      selected = c(as.character(unique(City_chars$Continent))),
+      options = list(`actions-box` = TRUE),
+      multiple = TRUE
+    )})
+  
+  # Base map
+  output$Map <- renderLeaflet({
+    leaflet() %>% addTiles()%>%
+      addProviderTiles(providers$Esri.WorldPhysical)
+  })
   
   # Filter the table
   table <- reactive({
-    x = input$cities 
-    if(length(x) != 0){
+    x = input$cities
+    y = input$clusters
+    z = input$continent
+    print(x)
+    print(y)
+    print(z)
+
+    if(length(x) != 0 & length(y) != 0 & length(z) != 0){
       if(x != "Aalborg"){
         d1 <- m[, ..x]
         d1 <- cbind(cities = m$c1, d1)
@@ -105,14 +136,65 @@ server <- function(input, output) {
         dtot <- na.omit(d2)
       }
       dtot <- dtot[order(dtot$distance),] 
+      # Filter cluster and continent
+      tcities <- City_chars[City_chars$Cluster %in% y & 
+                                      City_chars$Continent %in% z,1]
+      dtot <- data.frame(dtot)
+      dtot <- dtot[dtot$cities %in% tcities,]
+      row.names(dtot) <- NULL
+      
+      #Plot map
+      tcity <- City_chars[City_chars$namesds %in% x,]
+      leafletProxy("Map", data = tcity) %>%
+        clearMarkers() %>%
+        clearShapes() %>%
+        addAwesomeMarkers(data = tcity,
+                          lng = ~ x, lat = ~ y,
+                          label = lapply(tcity$City, htmltools::HTML),
+                          icon = makeAwesomeIcon(
+                            #icon = "ship",
+                            markerColor = "red",
+                            library = "fa",
+                            iconColor = "#FFFFFF"
+                          )) %>%
+        flyToBounds(~min(x), ~min(y), ~max(x), ~max(y))
+      
+      #Return dataset
+      
       dtot
     }
   })
   
   # Plot table
   output$table <- DT::renderDataTable({
-    datatable(table(), filter = 'top')
+    datatable(table(), options = list(lengthChange = FALSE, searching = FALSE) )
   })
+  
+  
+  observe({
+    tdt <- table()[input$table_rows_selected,]
+    print(tdt)
+# 
+#     if(!is.null(tdt)){
+# 
+#     tcity <- City_chars[City_chars$namesds %in% tdt$cities,]
+#     leafletProxy("Map", data = tcity) %>%
+#       clearMarkers() %>%
+#       clearShapes() %>%
+#       addAwesomeMarkers(data = tcity,
+#                         lng = ~ x, lat = ~ y,
+#                         label = lapply(tcity$City, htmltools::HTML),
+#                         icon = makeAwesomeIcon(
+#                           #icon = "ship",
+#                           markerColor = "red",
+#                           library = "fa",
+#                           iconColor = "#FFFFFF"
+#                         )) %>%
+#       flyToBounds(~min(x), ~min(y), ~max(x), ~max(y))
+#     }
+  })
+  
+
   
 }
 
