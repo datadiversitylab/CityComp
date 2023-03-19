a0721884ad1f38836a783061920baa0a6a1c2127
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-03-19 11:42:15 -0700

    Full functionality included

--- city_app.R
+++ city_app.R
@@ -9,6 +9,8 @@ library(shinybusy)
 library(dashboardthemes)
 library(reshape2)
 library(shinyWidgets)
+library(leaflet.extras)
+library(plotly)
 
 ## Global
 #m <- read_csv("data/pca_dists2.csv.gz")
@@ -21,6 +23,11 @@ colnames(City_chars)[2] <- "Continent"
 
 coords <- read.csv("data/cities_coords.csv")
 City_chars <- cbind.data.frame(namesds,City_chars, coords)
+
+#PCA
+dataScaled <- read.csv("data/cities_scaled.csv")
+pc <- princomp(dataScaled[,-c(1:2)])
+PCAdata <- data.frame(pc$scores,"cluster"=factor(City_chars$Cluster))
 
 ### UI
 header <- dashboardHeader(title = "City comparison",
@@ -39,7 +46,7 @@ body <- dashboardBody(
   fluidRow(
     column(width = 4,
            box(width = NULL,title = tagList(shiny::icon("info-circle",class = 'fa-lg'), "About the app"), solidHeader = T, collapsible = T, status = 'primary',
-               strong("CityComp"),"is an interactive shiny app which allows you to compare cities across the globe."
+               strong("CityComp"),"is an interactive shiny app that allows you compare the distances between cities across the globe. This app is based upon analyses presented in Arechiga et al. (in preparation)."
            ),
            box(width = NULL, title = tagList(shiny::icon("filter",class = 'fa-lg'), "Filters") ,
                solidHeader = T, collapsible = T, status = 'primary',
@@ -47,18 +54,36 @@ body <- dashboardBody(
                uiOutput("continent"),
                uiOutput("clusters")
            ),
-           box(width = NULL, title = tagList(shiny::icon("laptop-code",class = 'fa-lg'), "Map") ,
+           box(width = NULL, title = tagList(shiny::icon("map",class = 'fa-lg'), "Map") ,
                solidHeader = T, collapsible = FALSE, status = 'primary',
                leafletOutput('Map',height = 200)),
            box(width = NULL, title = tagList(shiny::icon("laptop-code",class = 'fa-lg'), "Code") ,
                solidHeader = T, collapsible = FALSE, status = 'primary',
+               a(icon("at",class = 'fa-lg'), ' Author 1: Cristian Román Palacios', href = 'cromanpa94.github.io', target = "_blank"),
+               br(),
+               a(icon("at",class = 'fa-lg'), ' Author 2: Kyle Arechiga', href = 'https://www.linkedin.com/in/kyle-arechiga-007a92132', target = "_blank"),
+               br(),
                a(icon("github",class = 'fa-lg'), ' GitHub repository', href = 'https://github.com/cromanpa94/city_app', target = "_blank")
                )
     ),
     column(width = 8,
-           box(width = NULL, solidHeader = TRUE,
-               DT::dataTableOutput("table"),
-           )
+           box(width = NULL, title = tagList(shiny::icon("check", class = 'fa-lg'), "PCA") ,
+               solidHeader = TRUE, collapsible = TRUE, status = 'primary',
+               h5("Let's get started comparing some cities! This app is designed such that you can
+                  quantify differences between cities across the globe. To begin comparing cities,
+                  please select your reference city using the filters on the left. Next, if you
+                  are interested in comparing that particular city to others in a particular region,
+                  please update the (1) continent-ish like filter or (2) the cluster-based one.
+                  Distances between cities can be visualized on the PCA plot in this app. Clusters are 
+                  based on KNN analyses. The app also includes a map that gets updated in accordance with
+                  the target city and the rows selected by clicking in the PCA-based distance table."),
+               ),
+           box(width = NULL, title = tagList(shiny::icon("tachometer-alt", class = 'fa-lg'), "PCA") ,
+               solidHeader = TRUE, collapsible = TRUE, status = 'primary',
+               plotlyOutput("plotPCA")),
+           box(width = NULL, title = tagList(shiny::icon("table",class = 'fa-lg'), "PCA-based distances") ,
+               solidHeader = T, collapsible = FALSE, status = 'primary',
+               DT::dataTableOutput("table"))
     )
   )
 )
@@ -78,7 +103,7 @@ server <- function(input, output) {
   output$cities <- renderUI({
     pickerInput(
       inputId = "cities",
-      label = "Select a city",
+      label = "Select target city",
       choices = unique(City_chars$namesds),
       options = list(`live-search`=TRUE)
     )})
@@ -86,7 +111,7 @@ server <- function(input, output) {
   output$clusters <- renderUI({
     pickerInput(
       inputId = "clusters",
-      label = "Select a  clusters",
+      label = "Select target cluster(s)",
       choices = c(as.character(unique(City_chars$Cluster))),
       selected = c(as.character(unique(City_chars$Cluster))),
       options = list(`actions-box` = TRUE),
@@ -97,7 +122,7 @@ server <- function(input, output) {
   output$continent <- renderUI({
     pickerInput(
       inputId = "continent",
-      label = "Select a  continent",
+      label = "Select target continent(s)",
       choices = c(as.character(unique(City_chars$Continent))),
       selected = c(as.character(unique(City_chars$Continent))),
       options = list(`actions-box` = TRUE),
@@ -106,19 +131,19 @@ server <- function(input, output) {
   
   # Base map
   output$Map <- renderLeaflet({
-    leaflet() %>% addTiles()%>%
-      addProviderTiles(providers$Esri.WorldPhysical)
+    leaflet(options = leafletOptions(
+                      attributionControl = FALSE)) %>% 
+      addTiles() %>%
+      addProviderTiles(providers$Esri.WorldPhysical) %>%
+      addFullscreenControl()
   })
   
   # Filter the table
   table <- reactive({
-    x = input$cities
+    x <<- input$cities
     y = input$clusters
     z = input$continent
-    print(x)
-    print(y)
-    print(z)
-
+    
     if(length(x) != 0 & length(y) != 0 & length(z) != 0){
       if(x != "Aalborg"){
         d1 <- m[, ..x]
@@ -152,15 +177,11 @@ server <- function(input, output) {
                           lng = ~ x, lat = ~ y,
                           label = lapply(tcity$City, htmltools::HTML),
                           icon = makeAwesomeIcon(
-                            #icon = "ship",
                             markerColor = "red",
                             library = "fa",
                             iconColor = "#FFFFFF"
                           )) %>%
         flyToBounds(~min(x), ~min(y), ~max(x), ~max(y))
-      
-      #Return dataset
-      
       dtot
     }
   })
@@ -173,26 +194,35 @@ server <- function(input, output) {
   
   observe({
     tdt <- table()[input$table_rows_selected,]
-    print(tdt)
-# 
-#     if(!is.null(tdt)){
-# 
-#     tcity <- City_chars[City_chars$namesds %in% tdt$cities,]
-#     leafletProxy("Map", data = tcity) %>%
-#       clearMarkers() %>%
-#       clearShapes() %>%
-#       addAwesomeMarkers(data = tcity,
-#                         lng = ~ x, lat = ~ y,
-#                         label = lapply(tcity$City, htmltools::HTML),
-#                         icon = makeAwesomeIcon(
-#                           #icon = "ship",
-#                           markerColor = "red",
-#                           library = "fa",
-#                           iconColor = "#FFFFFF"
-#                         )) %>%
-#       flyToBounds(~min(x), ~min(y), ~max(x), ~max(y))
-#     }
+    tcity <- City_chars[City_chars$namesds %in% c(tdt$cities, x),]
+
+    if(nrow(tcity)){
+     leafletProxy("Map", data = tcity) %>%
+        clearMarkers() %>%
+        clearShapes() %>%
+       addAwesomeMarkers(data = tcity,
+                         lng = ~ x, lat = ~ y,
+                         label = lapply(tcity$City, htmltools::HTML),
+                         icon = makeAwesomeIcon(
+                           markerColor = "red",
+                           library = "fa",
+                           iconColor = "#FFFFFF"
+                         )) %>%
+       flyToBounds(~min(x), ~min(y), ~max(x), ~max(y))
+     }
   })
+  
+  # PCA
+  
+  output$plotPCA <- renderPlotly({
+  p <- plot_ly(PCAdata,x=~Comp.1,y=~Comp.2,text=~City_chars$City,
+               mode="markers",color = ~cluster)
+  p <- layout(p,title="PCA Clusters",
+              xaxis=list(title="PC1"),
+              yaxis=list(title="PC2"))
+  p
+  })
+  
   
 
   
